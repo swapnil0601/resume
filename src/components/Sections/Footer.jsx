8ac1868b755e053eefbe2a4fd99aa3d4077b70c6
Swapnil Sahoo <workforswapnil06@gmail.com>
2023-09-20 17:22:46 +0530

    Section Added - Footer

---
+++ src/components/Sections/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "../../styles/Footer.css";
+const Footer = () => {
+  return (
+    <footer>
+      <nav>
+        <div class="nav-links-container">
+          <ul class="nav-links">
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#experience">Experience</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <p>Copyright &#169; 2023 John Doe. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
